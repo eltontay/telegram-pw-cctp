0af596d4130e8f58366975b5acec96e4fffb9afb
Elton Tay <57287843+eltontay@users.noreply.github.com>
2025-02-06 17:02:10 +0000

    Assistant checkpoint: Update CCTP configuration with all supported networks
    Assistant generated file changes:
    - src/config/cctp.js: Update CCTP configuration with all supported networks
    
    ---
    
    User prompt:
    
    ❌ Error: Cannot read properties of undefined (reading 'tokenMessenger')

--- src/config/cctp.js
+++ src/config/cctp.js
@@ -3,7 +3,9 @@ const CCTP_CONFIG = {
   domains: {
     'ETH-SEPOLIA': 0,
     'AVAX-FUJI': 1,
-    'BASE-SEPOLIA': 6
+    'ARB-SEPOLIA': 3,
+    'BASE-SEPOLIA': 6,
+    'MATIC-AMOY': 7
   },
   contracts: {
     'ETH-SEPOLIA': {
@@ -13,6 +15,24 @@ const CCTP_CONFIG = {
       message: '0xbac0179bb358a8936169a63408c8481d582390c4'
     },
     'AVAX-FUJI': {
+      tokenMessenger: '0x8fe6b999dc680ccfdd5bf7eb0974218be2542daa',
+      messageTransmitter: '0xe737e5cebeeba77efe34d4aa090756590b1ce275',
+      tokenMinter: '0xb43db544e2c27092c107639ad201b3defabcf192',
+      message: '0xbac0179bb358a8936169a63408c8481d582390c4'
+    },
+    'ARB-SEPOLIA': {
+      tokenMessenger: '0x8fe6b999dc680ccfdd5bf7eb0974218be2542daa',
+      messageTransmitter: '0xe737e5cebeeba77efe34d4aa090756590b1ce275',
+      tokenMinter: '0xb43db544e2c27092c107639ad201b3defabcf192',
+      message: '0xbac0179bb358a8936169a63408c8481d582390c4'
+    },
+    'BASE-SEPOLIA': {
+      tokenMessenger: '0x8fe6b999dc680ccfdd5bf7eb0974218be2542daa',
+      messageTransmitter: '0xe737e5cebeeba77efe34d4aa090756590b1ce275',
+      tokenMinter: '0xb43db544e2c27092c107639ad201b3defabcf192',
+      message: '0xbac0179bb358a8936169a63408c8481d582390c4'
+    },
+    'MATIC-AMOY': {
       tokenMessenger: '0x8fe6b999dc680ccfdd5bf7eb0974218be2542daa',
       messageTransmitter: '0xe737e5cebeeba77efe34d4aa090756590b1ce275',
       tokenMinter: '0xb43db544e2c27092c107639ad201b3defabcf192',
